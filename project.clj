03281063a016ba775a899501ae8540e5174ed1ad
Pyunghyuk Yoo <yoophi@gmail.com>
2016-03-31 10:36:11 +0900

    compojure/routes samples

--- project.clj
+++ project.clj
@@ -6,5 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.4.0"]
                  [metosin/ring-http-response "0.6.5"]
-                 [ring-middleware-format "0.7.0"]]
+                 [ring-middleware-format "0.7.0"]
+                 [compojure "1.4.0"]]
   :main ring-app.core)
--- src/ring_app/core.clj
+++ src/ring_app/core.clj
@@ -1,12 +1,21 @@
 (ns ring-app.core
   (:require [ring.adapter.jetty :as jetty]
+            [compojure.core :as compojure]
             [ring.util.http-response :as response]
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.middleware.format :refer [wrap-restful-format]]))
 
-(defn handler [request]
+(defn response-handler [request]
   (response/ok
-    {:result (-> request :params :id)}))
+    (str "<html><body> your IP is: "
+         (:remote-addr request)
+         "</body></html>")))
+
+(def handler
+  (compojure/routes
+    (compojure/GET "/" request response-handler)
+    (compojure/GET "/:id" [id] (str "<p>the id is " id "</p>"))
+    (compojure/POST "/json" [id] (response/ok {:result id}))))
 
 (defn wrap-nocache [handler]
   (fn [request]
